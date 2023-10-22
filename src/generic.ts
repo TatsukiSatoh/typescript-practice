fb71185ee0ac36bc53b4254dd3c96f3db439f6f4
ju12yw <76781368+ju12yw@users.noreply.github.com>
2023-10-22 11:39:53 +0900

    type generic

---
+++ src/generic.ts
@@ -0,0 +1,62 @@
+type User<T> = {
+    name: string
+    child: T
+}
+
+const testUser: User<number> = {
+    name: "Taro",
+    child: 1
+}
+
+type Family<Parent, Child> = {
+    mother: Parent,
+    father: Parent,
+    child: Child
+}
+
+const testFamily: Family<number, string> = {
+    mother: 1,
+    father: 2,
+    child: "100"
+}
+
+// 部分型関係による型引数の制約
+type HasName = {
+    name: string
+}
+
+type Family2<Parent extends HasName, Child extends HasName> = {
+    mother: Parent,
+    father: Parent,
+    child: Child
+}
+
+// 下記コードはエラー
+// type T = Family2<number, string>
+
+type Animal = {
+    name: string
+}
+
+type Human = {
+    name: string,
+    age?: number
+}
+
+const testFamily2: Family2<Animal, Human> = {
+    mother: {
+        name: "Test"
+    },
+    father: {
+        name: "Test2"
+    },
+    child: {
+        name: "Test3"
+    }
+}
+
+type Family3<Parent = Animal, Child = Human> = {
+    mother: Parent,
+    father: Parent,
+    child: Child
+}
